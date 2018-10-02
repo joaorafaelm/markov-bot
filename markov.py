ccfb18e1ea90d0845de1c380e19e86431131839c
Sujanani <ansujanani@hotmail.com>
2018-10-02 09:46:38 +0530

    fixed bug that reads past length

--- markov.py
+++ markov.py
@@ -63,7 +63,9 @@ def remove_messages(message):
 @bot.message_handler(commands=['version'])
 def get_repo_version(message):
     hash_len = 7
-    commit_hash = environ.get('HEROKU_SLUG_COMMIT')[:hash_len]
+    commit_hash = environ.get('HEROKU_SLUG_COMMIT', '')
+    if len(commit_hash) > 0:
+	commit_hash = commit_hash[:hash_len]
     bot.reply_to(message, commit_hash)
 
 
