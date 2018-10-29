f97a809a2a1b05b6b4b6e9c6de0dff490da56ad2
Kaz Wolfe <root@kazwolfe.io>
2018-10-29 11:57:18 -0700

    Fix comment left in PR - better check for empty messages
    Side note: thank you, I did not know this existed.

--- filters.py
+++ filters.py
@@ -11,7 +11,7 @@ filters = list(filter(None, [PATTERNS.get(f, '') for f in settings.FILTERS]))
 
 def message_filter(message):
     # If the message has no text, we just ignore it.
-    if message.text is None:
+    if not message.text:
         return False
 
     return not any(re.search(f, message.text, re.I) for f in filters)
