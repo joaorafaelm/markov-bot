c1db1b818c634906c0e40783798627f8871e9c63
Kaz Wolfe <root@kazwolfe.io>
2018-10-29 08:59:27 -0700

    Fix test coverage drop in filter test
    - Yes, i'm too lazy to test locally so i'll keep wasting commits. Like a real developer. :thonkfingerguns:

--- test/test_filters.py
+++ test/test_filters.py
@@ -7,3 +7,8 @@ def test_message_filter(message):
     assert filters.message_filter(message) is True
     message.text = 'Hello, World'
     assert filters.message_filter(message) is False
+
+
+def test_empty_message(message):
+    message.text = None
+    assert filters.message_filter(message) is False
